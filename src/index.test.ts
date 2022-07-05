62ecf8db5ab30390c856037860833edb831177d3
JunQu <junjv@outlook.com>
2022-07-05 23:10:35 +0800

    feat: add tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,8 +3,15 @@ import { numsToTree } from './index'
 
 describe('tests for numsToTree', () => {
   it('Full tree', () => {
-    const arr = [1, 2, 3, 4, 5, 6, 7] // 2**n - 1
-    const tree = {
+    const arr1 = [1, 2, 3]
+    const arr2 = [1, 2, 3, 4, 5, 6, 7] // 2**n - 1
+
+    const tree1 = {
+      val: 1,
+      left: { val: 2, left: null, right: null },
+      right: { val: 3, left: null, right: null },
+    }
+    const tree2 = {
       val: 1,
       left: {
         val: 2,
@@ -33,8 +40,74 @@ describe('tests for numsToTree', () => {
         },
       },
     }
-    expect(numsToTree(arr)).toEqual(tree)
+
+    expect(numsToTree(arr1)).toEqual(tree1)
+    expect(numsToTree(arr2)).toEqual(tree2)
   })
+
+  it("only left nodes", () => {
+    const arr1 =  [1,2,null,3]
+
+    const tree1 = {
+      val: 1,
+      right:null,
+      left:{
+        val: 2,
+        right: null,
+        left: {
+          val:3,
+          left: null,
+          right: null
+        }
+      }
+    }
+
+    expect(numsToTree(arr1)).toEqual(tree1)
+  });
+
+  it("only right nodes", () => {
+    const arr1 = [1,null,2,null,3]
+
+    const tree1 = {
+      val: 1,
+      left:null,
+      right:{
+        val: 2,
+        left: null,
+        right: {
+          val:3,
+          left: null,
+          right: null
+        }
+      }
+    }
+
+    expect(numsToTree(arr1)).toEqual(tree1)
+  });
+
+  it("trees of different shapes", () => {
+    const arr1 = [1, 2, null, 3, null, null, 4, 5, 6, null]
+
+    const tree1 = {
+      val: 1,
+      left: {
+        val: 2,
+        left: {
+          val: 3,
+          left: null,
+          right: {
+            val: 4,
+            left: { val: 5, left: null, right: null },
+            right: { val: 6, left: null, right: null },
+          },
+        },
+        right: null,
+      },
+      right: null,
+    }
+
+    expect(numsToTree(arr1)).toEqual(tree1)
+  });
 })
 
 // from https://support.leetcode.com/hc/en-us/articles/360011883654-What-does-1-null-2-3-mean-in-binary-tree-representation-
@@ -43,44 +116,124 @@ describe('some test for leetcode', () => {
     const arr1 = []
     const arr2 = [1, 2, 3]
     const arr3 = [1, null, 2, 3]
-    const arr4 = [5, 4, 7, 3, null, 2, null, -1, null, 9]
 
     const tree1 = null
     const tree2 = { val: 1, left: { val: 2, left: null, right: null }, right: { val: 3, left: null, right: null } }
     const tree3 = { val: 1, left: null, right: { val: 2, left: { val: 3, left: null, right: null }, right: null } }
-    const tree4 = {
+
+    expect(numsToTree(arr1)).toEqual(tree1)
+    expect(numsToTree(arr2)).toEqual(tree2)
+    expect(numsToTree(arr3)).toEqual(tree3)
+  })
+
+  it('more nodes tree', () => {
+    const arr1 = [1, 3, null, null, 2]
+    const arr2 = [3, 1, 4, null, null, 2]
+    const arr3 = [5, 4, 7, 3, null, 2, null, -1, null, 9]
+
+    const tree1 = {
+      val: 1,
+      right: null,
+      left: {
+        val: 3,
+        left: null,
+        right: {
+          val: 2,
+          left: null,
+          right: null,
+        },
+      },
+    }
+    const tree2 = {
+      val: 3,
+      left: {
+        val: 1,
+        left: null,
+        right: null,
+      },
+      right: {
+        val: 4,
+        right: null,
+        left: {
+          val: 2,
+          right: null,
+          left: null,
+        },
+      },
+    }
+    const tree3 = {
       val: 5,
       right: {
         val: 7,
-        right:null,
+        right: null,
         left: {
-          val:2,
+          val: 2,
           right: null,
           left: {
             val: 9,
-            right:null,
-            left:null
-          }
-        }
+            right: null,
+            left: null,
+          },
+        },
       },
       left: {
         val: 4,
         right: null,
         left: {
-          val:3,
+          val: 3,
           right: null,
-          left : {
+          left: {
             val: -1,
             left: null,
-            right: null
-          }
+            right: null,
+          },
+        },
+      },
+    }
+
+    expect(numsToTree(arr1)).toEqual(tree1)
+    expect(numsToTree(arr2)).toEqual(tree2)
+    expect(numsToTree(arr3)).toEqual(tree3)
+  })
+})
+
+describe('conditions for empty ', () => {
+  it('tree will be null', () => {
+    const arr1 = []
+    const arr2 = [null]
+    const arr3 = ['a']
+    const arr4 = [[]]
+    const arr5 = null
+    const arr6 = [-1, 2, 3, null, 5, 7, 8]
+
+    expect(numsToTree(arr1)).toBeNull()
+    expect(numsToTree(arr2)).toBeNull()
+    expect(numsToTree(arr3 as [])).toBeNull()
+    expect(numsToTree(arr4 as [])).toBeNull()
+    expect(numsToTree(arr5)).toBeNull()
+    expect(numsToTree(arr6, 3)).toBeNull()
+  })
+
+  it('tree will show part of elements', () => {
+    const arr1 = [1, null, null, 2]
+    const arr2 =  [1,2,null,3,null,null, null,null,4,5,6,null]
+
+    const tree1 = { val: 1, left: null, right: null }
+    const tree2 = {
+      val: 1,
+      right:null,
+      left:{
+        val: 2,
+        right: null,
+        left: {
+          val:3,
+          left: null,
+          right: null
         }
       }
     }
 
     expect(numsToTree(arr1)).toEqual(tree1)
     expect(numsToTree(arr2)).toEqual(tree2)
-    expect(numsToTree(arr3)).toEqual(tree3)
-    expect(numsToTree(arr4)).toEqual(tree4)
   })
 })
