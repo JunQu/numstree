5217c827fefc6e91fbe4f838c37d8f5402ab8feb
JunQu <junjv@outlook.com>
2022-07-05 17:47:55 +0800

    feat:add comments

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,9 @@ function isUndefined(prop: any): boolean {
   return typeof prop === 'undefined'
 }
 
+/**
+ * valid elements is numbers like -1,0, 1,2.2,3.1415..., other values will covert to null
+ */
 function isNum(prop: any): boolean {
   return typeof prop === 'number' && prop === prop
 }
@@ -24,6 +27,12 @@ function createNode(val: number, id: number, left?: TreeType | null, right?: Tre
   }
 }
 
+/**
+ * build a binary tree from array that only has number or null elements
+ * root start by rootIndex, if you set this, the root node is it
+ * before you want change this, please see this article first to compatible with it:
+ * https://support.leetcode.com/hc/en-us/articles/360011883654-What-does-1-null-2-3-mean-in-binary-tree-representation-
+ */
 export function numsToTree(arr: (number | null)[], rootIndex = 0): NodeType | null {
   if (!arr.length || typeof arr[rootIndex] !== 'number') {
     return null
@@ -37,10 +46,18 @@ export function numsToTree(arr: (number | null)[], rootIndex = 0): NodeType | nu
   return root
 }
 
+/**
+ * add every nums to the tree
+ * first find the node's parent Node
+ * then insert the Node
+ */
 function buildTreeByIndex(root: TreeType, rootIndex: number, arr: (number | null)[]): void {
+  // cache previous level nodes as next leve parents nodes
   let parents: number[] = [rootIndex]
+  // cache first child array index to avoid copy values
   let firstChildIndex = rootIndex + 1
 
+  // when last leaves found, parent is empty
   while (parents.length) {
     const childrenCounter = 2 * parents.length
     let parentCounter = 0
@@ -70,12 +87,14 @@ function buildTreeByIndex(root: TreeType, rootIndex: number, arr: (number | null
       }
     }
 
+    // remove redundant parent nodes
     parents.length = parentCounter
     firstChildIndex += childrenCounter
   }
 }
 
 /**
+ * find the Node place and insert it
  * there are lots of place in insertNodeToTree function for optimization
  * because it repeated the search many times
  * this is where the current performance bottleneck is biggest
@@ -105,6 +124,9 @@ function insertNodeToTree(
   insertNodeToTree(root.right, parentIndex, node, isLeftLeaf)
 }
 
+/**
+ * remove a property in the whole tree
+ */
 function removeIdFromTree(root: TreeType | null, property: string): void {
   if (!root) {
     return
